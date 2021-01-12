80834de21c42a6aee18f602ba7f2fa130c5c6b26
Rizalabdilah608 <77323909+Rizalabdilah608@users.noreply.github.com>
2021-01-12 15:54:55 +0700

    Add files via upload

---
+++ KuotaGratis.py
@@ -0,0 +1,23 @@
+print"======================================="
+print"YT = Rizal Abdilah"
+print"IG = Rizalabdilah608"
+print"======================================="
+
+print"---Kuota Gratis All Operator---"
+
+Nomor = input("Masukan Nomor Anda: +62")
+
+Kuota = input("Angka Kuota Yang Anda Inginkan: ")
+
+print"1. MB"
+print"2. GB"
+print"0. Close"
+
+menu = input("Masukan Input: ")
+
+if menu==1:
+    print"Tunggu 1 Menit! Sabar Ya Bang!"
+elif menu==2:
+    print"Tunggu 2 Menit! Sabar Ya Bang!"
+elif menu==0:
+    print"Bye!"
